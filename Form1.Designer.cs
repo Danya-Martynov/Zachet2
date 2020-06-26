5b53850fee0eb19f13796589667d448cf1b43a09
Danya-Martynov <66323130+Danya-Martynov@users.noreply.github.com>
2020-06-26 11:04:07 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbNum1 = new System.Windows.Forms.TextBox();
+            this.txtbNum2 = new System.Windows.Forms.TextBox();
+            this.txtbNum4 = new System.Windows.Forms.TextBox();
+            this.txtbNum3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnVychisl = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnR = new System.Windows.Forms.Button();
+            this.btnG = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbNum1
+            // 
+            this.txtbNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtbNum1.Location = new System.Drawing.Point(41, 102);
+            this.txtbNum1.Name = "txtbNum1";
+            this.txtbNum1.Size = new System.Drawing.Size(100, 24);
+            this.txtbNum1.TabIndex = 0;
+            this.txtbNum1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbNum1_KeyPress);
+            // 
+            // txtbNum2
+            // 
+            this.txtbNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtbNum2.Location = new System.Drawing.Point(41, 149);
+            this.txtbNum2.Name = "txtbNum2";
+            this.txtbNum2.Size = new System.Drawing.Size(100, 24);
+            this.txtbNum2.TabIndex = 1;
+            this.txtbNum2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // txtbNum4
+            // 
+            this.txtbNum4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtbNum4.Location = new System.Drawing.Point(41, 246);
+            this.txtbNum4.Name = "txtbNum4";
+            this.txtbNum4.Size = new System.Drawing.Size(100, 24);
+            this.txtbNum4.TabIndex = 3;
+            this.txtbNum4.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.txtbNum4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // txtbNum3
+            // 
+            this.txtbNum3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtbNum3.Location = new System.Drawing.Point(41, 198);
+            this.txtbNum3.Name = "txtbNum3";
+            this.txtbNum3.Size = new System.Drawing.Size(100, 24);
+            this.txtbNum3.TabIndex = 2;
+            this.txtbNum3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите 4 числа:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(128, 331);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Вычислить наибольшее чётное";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnVychisl
+            // 
+            this.btnVychisl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnVychisl.Location = new System.Drawing.Point(190, 361);
+            this.btnVychisl.Name = "btnVychisl";
+            this.btnVychisl.Size = new System.Drawing.Size(100, 38);
+            this.btnVychisl.TabIndex = 6;
+            this.btnVychisl.Text = "Вычислить";
+            this.btnVychisl.UseVisualStyleBackColor = true;
+            this.btnVychisl.Click += new System.EventHandler(this.btnVychisl_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(231, 411);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 7;
+            // 
+            // btnR
+            // 
+            this.btnR.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnR.Location = new System.Drawing.Point(215, 59);
+            this.btnR.Name = "btnR";
+            this.btnR.Size = new System.Drawing.Size(75, 32);
+            this.btnR.TabIndex = 8;
+            this.btnR.Text = "red";
+            this.btnR.UseVisualStyleBackColor = true;
+            this.btnR.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnG
+            // 
+            this.btnG.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnG.Location = new System.Drawing.Point(296, 59);
+            this.btnG.Name = "btnG";
+            this.btnG.Size = new System.Drawing.Size(75, 32);
+            this.btnG.TabIndex = 9;
+            this.btnG.Text = "green";
+            this.btnG.UseVisualStyleBackColor = true;
+            this.btnG.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnB.Location = new System.Drawing.Point(378, 58);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(75, 33);
+            this.btnB.TabIndex = 10;
+            this.btnB.Text = "blue";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 450);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnG);
+            this.Controls.Add(this.btnR);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnVychisl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbNum4);
+            this.Controls.Add(this.txtbNum3);
+            this.Controls.Add(this.txtbNum2);
+            this.Controls.Add(this.txtbNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbNum1;
+        private System.Windows.Forms.TextBox txtbNum2;
+        private System.Windows.Forms.TextBox txtbNum4;
+        private System.Windows.Forms.TextBox txtbNum3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnVychisl;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnR;
+        private System.Windows.Forms.Button btnG;
+        private System.Windows.Forms.Button btnB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtbNum1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39 || e.KeyChar >= 46) && number != 47 && number != 61)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39 || e.KeyChar >= 46) && number != 47 && number != 61) 
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39 || e.KeyChar >= 46) && number != 47 && number != 61) 
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39 || e.KeyChar >= 46) && number != 47 && number != 61) 
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnVychisl_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(txtbNum1.Text);
+            int num2 = Convert.ToInt32(txtbNum2.Text);
+            int num3 = Convert.ToInt32(txtbNum3.Text);
+            int num4 = Convert.ToInt32(txtbNum4.Text);
+            int c1 = 0;
+            int c2 = 0;
+            int c3 = 0;
+            int c4 = 0;
+            int max1, max2, max = 0;
+
+            if (num1 % 2 == 0)
+            {
+                c1 = num1;
+            }
+            if (num2 % 2 == 0)
+            {
+                c2 = num2;
+            }
+            if (num3 % 2 == 0)
+            {
+                c3 = num3;
+            }
+            if (num4 % 2 == 0)
+            {
+                c4 = num4;
+            }
+            if (c1 > c2)
+            {
+                max1 = c1;
+            }
+            else max1 = c2;
+            if (c3 > c4)
+            {
+                max2 = c3;
+            }
+            else max2 = c4;
+            if (max1 > max2)
+                max = max1;
+            else max = max2;
+
+            if (max != 0)   
+            lblResult.Text = Convert.ToString(max);
+            else lblResult.Text = "Not Found";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Green;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Blue;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
